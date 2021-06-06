e434c206dd62416c80b34b48a4faccfce6ce3ded
Aakash <59740618+aakashbamba@users.noreply.github.com>
2021-06-06 20:37:33 +0530

    Dice

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,24 @@
+var randomNumber1= Math.floor(Math.random()*6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 +".png"; //images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// Changing H1
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player1 Wins!";
+}
+else if(randomNumber2 > randomNumber1)
+{
+    document.querySelector("h1").innerHTML = "Player2 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
